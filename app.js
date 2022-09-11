1ded577e18091a3811604b33f3e9f23766e55865
Aymon2016 <mdaiman2016@gmail.com>
2022-09-11 13:13:59 +0600

    first commit

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const router = require("./Routes/router");
 const Products = require("./models/productsSchema");
 const jwt = require("jsonwebtoken");
 const cors=require('cors')
+const path= require('path')
 
 
 // middleware
@@ -20,14 +21,18 @@ app.use(cookieParser());
 
 
 app.use(router);
-app.get("/",(req,res)=>{
-    res.send("your server is running");
-});
+// app.get("/",(req,res)=>{
+//     res.send("your server is running");
+    
+// });
 
 
-// if(process.env.NODE_ENV == "production"){
-//     app.use(express.static("client/build"));
-// }
+if(process.env.NODE_ENV == "production"){
+    app.use(express.static("client/build"));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(port,()=>{
     console.log(`your server is running on port ${port} `);
